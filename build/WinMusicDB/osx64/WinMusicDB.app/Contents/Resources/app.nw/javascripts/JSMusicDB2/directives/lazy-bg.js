baef4dd73bffabefecd9037646183e11e48695bf
Lucien Immink <lucien.immink@isaac.nl>
2014-12-18 09:16:51 +0100

    Updated to node-webkit 0.11.3
    Former-commit-id: 2e7321289b9398c1c53f981a3b118302ebdb10e5 [formerly 76a34ee885b075eaeac01014c152cff9e1acbf68]
    Former-commit-id: 46addea1f902a768e272386df006006313f49a6a

--- build/WinMusicDB/osx64/WinMusicDB.app/Contents/Resources/app.nw/javascripts/JSMusicDB2/directives/lazy-bg.js
+++ build/WinMusicDB/osx64/WinMusicDB.app/Contents/Resources/app.nw/javascripts/JSMusicDB2/directives/lazy-bg.js
@@ -344,7 +344,7 @@ jsmusicdb.directive("bnLazyBg", function($window, $document, $http, ImageService
 			} else {
 				ImageService.getArt(source, function (json) {
 					if (json && json.images.length > 0) {
-						cachedResult = json.images[0].url || "images/nocover.png";
+						cachedResult = json.images[0].url || "images/nocover.webp";
 						setImage(cachedResult);
 					} else {
 						ImageService.getLastFMArt(source, function (json) {
@@ -352,11 +352,11 @@ jsmusicdb.directive("bnLazyBg", function($window, $document, $http, ImageService
 							if (json) {
 								angular.forEach(json.image, function (e) {
 									if (e.size === "mega") {
-										cachedResult = e["#text"] || "images/nocover.png";
+										cachedResult = e["#text"] || "images/nocover.webp";
 									}
 								});
 							} else {
-								cachedResult = "images/nocover.png";
+								cachedResult = "images/nocover.webp";
 							}
 							setImage(cachedResult);
 						});
