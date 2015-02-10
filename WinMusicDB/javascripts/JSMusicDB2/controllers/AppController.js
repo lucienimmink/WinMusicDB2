ac6cb08b9061b8de713ad0498c42a7f817c7f52c
Lucien Immink <lucien.immink@isaac.nl>
2015-02-10 11:38:12 +0100

    [TASK] Changed colour

--- WinMusicDB/javascripts/JSMusicDB2/controllers/AppController.js
+++ WinMusicDB/javascripts/JSMusicDB2/controllers/AppController.js
@@ -21,7 +21,7 @@ function($scope, $http, $rootScope, $location, $routeParams, $modal, RestService
 		cursorwidth : 5,
 		cursorborderradius : "0px",
 		horizrailenabled : false,
-		cursorcolor : '#5A8DBA',
+		cursorcolor : '#8DA7CC',
 		cursorborder : '1px solid #607d8b'
 	});
 
